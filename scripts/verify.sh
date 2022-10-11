dbeb057f92c0e80b4e1281461f51c1da83a40b72
Jeremy Epstein <jazepstein@gmail.com>
2022-10-11 16:44:11 +1100

    fix bandit

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -5,6 +5,6 @@ $HOME/.local/bin/poetry run black fastapi_oracle tests --check
 $HOME/.local/bin/poetry run isort --check-only fastapi_oracle tests
 $HOME/.local/bin/poetry run flake8 fastapi_oracle tests
 $HOME/.local/bin/poetry run mypy fastapi_oracle tests
-$HOME/.local/bin/poetry run bandit fastapi_oracle tests
+$HOME/.local/bin/poetry run bandit -c pyproject.toml -r fastapi_oracle tests
 
 set +e
