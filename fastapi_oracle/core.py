2acec9ed5496835ce12cca97387a3f1c6b526b74
Jeremy Epstein <jazepstein@gmail.com>
2023-01-10 13:42:48 +1100

    Handle errors on DB pool close better

--- fastapi_oracle/core.py
+++ fastapi_oracle/core.py
@@ -1,4 +1,3 @@
-import asyncio
 import time
 from typing import AsyncGenerator
 
@@ -18,44 +17,66 @@ from fastapi_oracle.constants import (
 )
 
 
-DB_POOL_LOCK = asyncio.Lock()
+async def close_db_pool(pool: AsyncPoolWrapper):  # pragma: no cover
+    """Close the DB connection pool."""
+    try:
+        await pool.close()
+    except DatabaseError as ex:
+        if "while trying to destroy the Session Pool" in f"{ex}":
+            logger.warning(
+                '"error occurred while trying to destroy the Session Pool" '
+                "was raised, when releasing the database connection pool - "
+                "this can happen when there are still busy connections - "
+                "suppressing this error, hopefully consuming code can then "
+                "continue gracefully"
+            )
+        elif "not connected" in f"{ex}":
+            logger.warning(
+                '"not connected" was raised, when releasing the database '
+                "connection pool - this can happen on release when the pool "
+                "has already been closed - assuming that that's what happened "
+                "in this case, therefore suppressing this error, so that "
+                "consuming code can continue gracefully"
+            )
+        else:
+            raise ex
 
 
 async def get_or_create_db_pool(
     settings: Settings,
 ) -> AsyncPoolWrapper:  # pragma: no cover
-    async with DB_POOL_LOCK:
-        pool_key = DbPoolKey(
-            settings.db_host,
-            settings.db_port,
-            settings.db_user,
-            settings.db_service_name,
-        )
+    """Get or create the DB connection pool."""
+    pool_key = DbPoolKey(
+        settings.db_host,
+        settings.db_port,
+        settings.db_user,
+        settings.db_service_name,
+    )
 
-        if pools.DB_POOLS.get(pool_key) is not None:
-            ttl = settings.db_conn_ttl
-            pool, created_time = pools.DB_POOLS[pool_key]
+    if pools.DB_POOLS.get(pool_key) is not None:
+        ttl = settings.db_conn_ttl
+        pool, created_time = pools.DB_POOLS[pool_key]
 
-            if ttl is not None and time.monotonic() - created_time >= ttl:
-                logger.info(
-                    "Closing the existing database connection pool because it is older "
-                    f"than {ttl} seconds"
-                )
-                await pool.close()
-            else:
-                return pool
+        if ttl is not None and time.monotonic() - created_time >= ttl:
+            logger.info(
+                "Closing the existing database connection pool because it is older "
+                f"than {ttl} seconds"
+            )
+            await close_db_pool(pool)
+        else:
+            return pool
 
-        pool = await create_pool(
-            host=settings.db_host,
-            port=f"{settings.db_port}",
-            user=settings.db_user,
-            password=settings.db_password,
-            service_name=settings.db_service_name,
-        )
-        pools.DB_POOLS[pool_key] = DbPoolAndCreatedTime(
-            pool=pool, created_time=time.monotonic()
-        )
-        return pools.DB_POOLS[pool_key].pool
+    pool = await create_pool(
+        host=settings.db_host,
+        port=f"{settings.db_port}",
+        user=settings.db_user,
+        password=settings.db_password,
+        service_name=settings.db_service_name,
+    )
+    pools.DB_POOLS[pool_key] = DbPoolAndCreatedTime(
+        pool=pool, created_time=time.monotonic()
+    )
+    return pools.DB_POOLS[pool_key].pool
 
 
 async def get_db_pool(
@@ -86,9 +107,9 @@ async def get_db_conn(
             logger.warning(
                 '"not connected" was raised, either when acquiring or when releasing '
                 "the database connection pool - this can happen on release when the "
-                "pool has already been force closed - assuming that that's what "
-                "happened in this case, therefore suppressing this error, so that "
-                "consuming code can continue gracefully"
+                "pool has already been closed - assuming that that's what happened in "
+                "this case, therefore suppressing this error, so that consuming code "
+                "can continue gracefully"
             )
         else:
             raise ex
@@ -109,16 +130,13 @@ async def get_db_cursor(
         yield DbPoolConnAndCursor(pool=pool, conn=conn, cursor=cursor)
 
 
-async def close_db_pools(
-    force: bool = False, interrupt: bool = False
-):  # pragma: no cover
+async def close_db_pools():  # pragma: no cover
     """Close the DB connection pools.
 
     This shouldn't need to be called manually in most cases, it's registered as a
     FastAPI shutdown function, so it will get called when the Python process ends.
     """
-    async with DB_POOL_LOCK:
-        for pool, _ in pools.DB_POOLS.values():
-            await pool.close(force=force, interrupt=interrupt)
+    for pool, _ in pools.DB_POOLS.values():
+        await close_db_pool(pool)
 
-        pools.DB_POOLS = {}
+    pools.DB_POOLS = {}
